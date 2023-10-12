0124e7bae2f155674223152ebb20268637c2318d
Petrik Diak <diak@petrik.hu>
2023-10-12 08:52:20 +0200

    asd

---
+++ KolcsonzoOOP/Dvd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolcsonzoOOP
+{
+	internal class Dvd : IKolcsonozheto
+	{
+		private string cim;
+		private int hossz;
+
+		public Dvd(string cim, int hossz)
+		{
+			this.cim = cim;
+			this.hossz = hossz;
+		}
+		
+		public string MegjelenitendoNev()
+		{
+			return $"{this.cim} ({hossz} perc)";
+		}
+		public int MeddigKolcsonozheto()
+		{
+			return 7;
+		}
+
+	}
+}
--- KolcsonzoOOP/IKolcsonozheto.cs
+++ KolcsonzoOOP/IKolcsonozheto.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 
 namespace KolcsonzoOOP
 {
-	internal class Program 
+	internal interface IKolcsonozheto
 	{
-		static void Main(string[] args)
-		{
-			
-		}
+		string MegjelenitendoNev();
+		int MeddigKolcsonozheto();
 	}
 }
